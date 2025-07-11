1ee7c0523cc0f913235f0bba73e8e642d7e4688b
ipatpat <ipatpat.com@gmail.com>
2025-07-11 14:54:31 +0800

    Revert "us_data_for_gen"
    This reverts commit 76761d8de39d418d05fff8f26e6ce544345045e4.

--- rdagent/scenarios/qlib/experiment/factor_data_template/generate.py
+++ rdagent/scenarios/qlib/experiment/factor_data_template/generate.py
@@ -1,10 +1,10 @@
 import qlib
 
-qlib.init(provider_uri="~/.qlib/qlib_data/us_data")
+qlib.init(provider_uri="~/.qlib/qlib_data/cn_data")
 
 from qlib.data import D
 
-instruments = D.instruments() # Use NASDAQ-100 as an example
+instruments = D.instruments()
 fields = ["$open", "$close", "$high", "$low", "$volume", "$factor"]
 data = D.features(instruments, fields, freq="day").swaplevel().sort_index().loc["2008-12-29":].sort_index()
 
